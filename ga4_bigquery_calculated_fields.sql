9155685aa7d8ccdc3ff04743ab40c5b79047a433
Gloria Yu <75326954+gloriayu728@users.noreply.github.com>
2026-06-11 11:03:37 -0400

    Reverted form submissions and clicks to applications to old formulas
    Reverted form submissions and clicks to applications to be user-based.

--- ga4_bigquery_calculated_fields.sql
+++ ga4_bigquery_calculated_fields.sql
@@ -96,7 +96,7 @@ END
 -- Metric: Clicks to Applications
 
 CASE  
-WHEN Event Name = 'click_application_undergrad' or Event Name = 'click_application_grad' THEN Sessions
+WHEN Event Name = 'click_application_undergrad' or Event Name = 'click_application_grad' THEN User Pseudo ID
 END
 
 -- Metric: Engaged Sessions
@@ -115,7 +115,7 @@ END
 -- Metric: Form Submissions 
 
 CASE 
-WHEN Event Name = 'form_submit_undergrad_rfi' or Event Name = 'form_submit_grad_rfi' THEN Sessions
+WHEN Event Name = 'form_submit_undergrad_rfi' or Event Name = 'form_submit_grad_rfi' THEN User Pseudo ID
 END 
 
 -- Metric: Form Submission Rate
